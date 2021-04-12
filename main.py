acfd000d47a360c547cd5ae084f5df6712f8e1b2
Asaf <asnav1999@gmail.com>
2021-04-12 03:03:26 +0300

    I'm not done I'm just tired Its 3:02 already and I gotta sleep GoodNight :)

--- main.py
+++ main.py
@@ -1,44 +1,137 @@
-# This is a sample Python script.
+def printMatrix(matrix):
+    for row in matrix:
+            print(row)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def matMultiply(A, B):
+    # multiply from the left: A * B.
+    result = []
+    for i in range(len(A)):
+        rowMat = []
+        for j in range(len(B[i])):
+            index = 0
+            for k in range(len(B)):
+                index += A[i][k] * B[k][j]
+            rowMat.append(index)
+        result.append(rowMat)
+    return result
 
 
-matrix4 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-matrix3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-def matReducer(A):
-    sum = 0
+def detCalculator(A): #it works!!!
     matLen = len(A)
-    currentRow = 0
-    # will calculate det of first row
-    # We assume that its a square matrix
-    for index in range(matLen):
-        newMat = []
-        currentCol = index
-        for i in range(matLen):
-            newMatRow = []
-            for j in range(matLen):
-                if i != currentRow and j != currentCol:
-                    newMatRow.append(A[i][j])
-            if len(newMatRow) > 0:
-                newMat.append(newMatRow)
-        if currentCol % 2 != 0:
-            pivot = -(A[currentRow][currentCol])
-        else:
-            pivot = A[currentRow][currentCol]
-        sum += pivot * detCalculator(newMat)
-    print(sum)
-
-
-def detCalculator(A):
-    if len(A) != 2:
+    if matLen < 2:
         return None
-    else:
+    if matLen == 2:
         return (A[0][0] * A[1][1]) - (A[0][1] * A[1][0])
+    else:
+        sum = 0
+        currentRow = 0
+        # will calculate det of first row
+        # We assume that its a square matrix
+        for currentCol in range(matLen):
+            newMat = [x[:] for x in A]
+            for row in range(matLen):
+                del newMat[row][currentCol]
+            del newMat[currentRow]
+            pivot = A[currentRow][currentCol]
+            if currentCol % 2 != 0:
+                pivot *= -1
+            printMatrix(newMat)
+            sum += pivot * detCalculator(newMat)
+        return sum
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    matReducer(matrix3)
+
+def makeIdentityMat(dimension):
+    # Works for square matrix only
+    identityMat = []
+    for i in range(dimension):
+        rowMat = []
+        for j in range(dimension):
+            if i == j:
+                rowMat.append(1)
+            else:
+                rowMat.append(0)
+        identityMat.append(rowMat)
+    return identityMat
 
 
+def inverse(matrix):
+    inverseMatrix = makeIdentityMat(len(matrix))
+    matrixCopy = [x[:] for x in matrix]
+    matrixSize = len(matrix)
+    for row in range(matrixSize):
+        if matrixCopy[row][row] == 0:
+        #for column in range(matrixSize):
+            print("im done for today, C ya tomorrow")
+    return 0
+
+
+
+def norm(matrix):
+    result = 0
+    for row in matrix:
+        currentRowSum = 0
+        for column in matrix[row]:
+            currentRowSum += abs(matrix[row][column])
+        if currentRowSum > result:
+            result = currentRowSum
+    return result
+
+
+def makeUpperTriangular(A):
+    dimension = len(A)
+    for i in range(dimension):
+        for j in range(i + 1):
+            # print("[", i, "]", " [", j, "]")
+            if i == j:
+                A = makePivotOne(i, A)
+            else:
+                A = makePivotZero(i, j, A)
+    return A
+
+
+def makePivotZero(i, j, A):
+    if A[j][j] == 0:
+        return A
+    I = makeIdentityMat(len(A))
+    I[i][j] = -A[i][j] / A[j][j]
+    return matMultiply(I, A)
+
+
+def makePivotOne(i, A):
+    if A[i][i] == 0:
+        return A
+    I = makeIdentityMat(len(A))
+    I[i][i] = 1 / A[i][i]
+    return matMultiply(I, A)
+
+
+def gaussianElimination(A):
+    A = makeUpperTriangular(A)
+    return A
+
+
+def calcMat(A):
+    if len(A) < 4:
+        return gaussianElimination(A)
+    else:
+        print("Use Lu")
+
+    # _______________________________________________________________
+
+
+Q = [[1, 3, 5, 9],
+     [1, 3, 1, 7],
+     [4, 3, 9, 7],
+     [5, 2, 0, 9]]
+
+Z = [[11, 3, 7],
+     [6.5, 12, 13],
+     [0, 0, 9]]
+
+S = [[1, 3, 2],
+     [2, 6, 4],
+     [1, 0, 3]]
+
+printMatrix(calcMat(Z))
+print(detCalculator(Z))
